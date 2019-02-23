f465f72fb2bddd72dfe9d9bf5f374140f5a4522a
LucyCosta <lucyellen.costa@ftd.com.br>
2019-02-23 12:03:13 -0300

    Corrigindo nomes

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,26 +1,26 @@
-function addBoll() 
+function addBall() 
 {
-    var boll = document.createElement("div");
-    boll.setAttribute("class", "boll");
+    var ball = document.createElement("div");
+    ball.setAttribute("class", "boll");
     var x =  Math.floor(Math.random() * 500);
     var y =  Math.floor(Math.random() * 400);
 
-    boll.setAttribute("style", "left:" + x +"px;top:"+y+"px;background-color:" + gera_cor() +";");
-    boll.setAttribute("onClick", "estourar(this)");
+    ball.setAttribute("style", "left:" + x +"px;top:"+y+"px;background-color:" + getColor() +";");
+    ball.setAttribute("onClick", "estourar(this)");
 
-    document.body.appendChild(boll);
+    document.body.appendChild(ball);
 }
 
-function gera_cor()
+function getColor()
 {
     var hexadecimais = '0123456789ABCDEF';
-    var cor = '#';
+    var color = '#';
 
     for (var i = 0; i < 6; i++ ) {
-        cor += hexadecimais[Math.floor(Math.random() * 16)];
+        color += hexadecimais[Math.floor(Math.random() * 16)];
     }
     
-    return cor;
+    return color;
 }
 
 function estourar(objeto) 
@@ -30,5 +30,5 @@ function estourar(objeto)
 
 function start() 
 {
-   setInterval(addBoll, 500);
+   setInterval(addBall, 500);
 }
